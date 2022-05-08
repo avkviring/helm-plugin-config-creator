19c401e4a050c8bdc10208a9967946451a890483
Kviring Aleksey <alex@kviring.com>
2022-05-08 04:56:46 +0300

    Initial commit

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 RUSTFLAGS="-C linker=x86_64-linux-musl-gcc -Ctarget-cpu=haswell -Ctarget-feature=+avx2" cargo build --release --target x86_64-unknown-linux-musl
+cp target/x86_64/release/cheetah-config-creator helm-plugin/cheetah-config-creator

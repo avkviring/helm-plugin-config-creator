4d5a0cc248dbeeddd2cabd318f1b5a76e624c4fb
Kviring Aleksey <alex@kviring.com>
2022-05-08 05:16:53 +0300

    Plugin.yaml в перенесен в корень. helm install не умеет работать с каталогами внутри git репозитория.

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 RUSTFLAGS="-C linker=x86_64-linux-musl-gcc -Ctarget-cpu=haswell -Ctarget-feature=+avx2" cargo build --release --target x86_64-unknown-linux-musl
 cargo build --release
-cp target/x86_64-unknown-linux-musl/release/cheetah-config-creator helm-plugin/cheetah-config-creator-linux
-cp target/release/cheetah-config-creator helm-plugin/cheetah-config-creator-macos
+cp target/x86_64-unknown-linux-musl/release/cheetah-config-creator cheetah-config-creator-linux
+cp target/release/cheetah-config-creator cheetah-config-creator-macos

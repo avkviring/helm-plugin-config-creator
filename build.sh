2303548de9f60c7e8063433d5e5aa98bb021e23f
Kviring Aleksey <alex@kviring.com>
2022-05-08 05:11:21 +0300

    Build plugin

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+#!/bin/bash
 RUSTFLAGS="-C linker=x86_64-linux-musl-gcc -Ctarget-cpu=haswell -Ctarget-feature=+avx2" cargo build --release --target x86_64-unknown-linux-musl
-cp target/x86_64/release/cheetah-config-creator helm-plugin/cheetah-config-creator
+cargo build --release
+cp target/x86_64-unknown-linux-musl/release/cheetah-config-creator helm-plugin/cheetah-config-creator-linux
+cp target/release/cheetah-config-creator helm-plugin/cheetah-config-creator-macos
